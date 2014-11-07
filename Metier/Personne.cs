5c4160306403835caf1e669816b0da0a5dcd9510
Silhris <crepu.alexandre@hotmail.fr>
2014-11-07 10:46:04 +0100

    init 2

--- Metier/Personne.cs
+++ Metier/Personne.cs
@@ -8,5 +8,26 @@ namespace Metier
 {
     class Personne
     {
+        private string nom;
+        private string prenom;
+        private string ville;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+
+        public string Ville
+        {
+            get { return ville; }
+            set { ville = value; }
+        }
+
     }
 }
