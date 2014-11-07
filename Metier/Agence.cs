5495a1d2a38a85177f462c9f2692e14b9a613cce
agory <gory.alexandre02@gmail.com>
2014-11-07 16:32:37 +0100

    test alex

---
+++ Metier/Agence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Agence
+    {
+        private string nom;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public Agence(string nom)
+        {
+            this.nom = nom;
+        }
+
+    }
+}
