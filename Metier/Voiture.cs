680d67c4c8a0ddaeda1f8e8119c3ea7b94ffcade
agory <gory.alexandre02@gmail.com>
2014-11-10 13:49:26 +0100

    ssd

--- Metier/Voiture.cs
+++ Metier/Voiture.cs
@@ -8,6 +8,51 @@ namespace Metier
 {
     class Voiture
     {
-        private string modele;
+        private string categorie;
+        private string immatriculation;
+        private string loueur;
+        private string nom;
+        private int puissance;
+        private DateTime dateMiseService;
+
+        public string Immatriculation
+        {
+            get { return immatriculation; }
+            set { immatriculation = value; }
+        }
+        public int Puissance
+        {
+            get { return puissance; }
+            set { puissance = value; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public DateTime DateMiseService
+        {
+            get { return dateMiseService; }
+            set { dateMiseService = value; }
+        }
+        private bool estLoue;
+
+        public string Loueur
+        {
+            get { return loueur; }
+            set { loueur = value; }
+        }
+        public string Categorie
+        {
+            get { return categorie; }
+            set { categorie = value; }
+        }
+
+        public bool EstLoue
+        {
+            get { return estLoue; }
+            set { estLoue = value; }
+        }
     }
 }
